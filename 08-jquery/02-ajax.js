45bda1141fbdb0bb8dbbce09581a080c2f6631bd
Erick-pdx <francoer@pdx.edu>
2024-11-26 21:17:21 -0800

    all exercises done

--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,51 +1,41 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
+const $app = $("#books");
+const $loading = $("#loading");
 
-const app = document.querySelector('#books');
-app.style.paddingLeft = 0;
-const loading = document.querySelector('#loading');
+$app.css("padding-left", 0);
 
 const addBookToDOM = (item) => {
   console.log(item);
-  let element = document.createElement('div');
-  let title = document.createElement('h4');
-  let author = document.createElement('p');
-  let published = document.createElement('p');
-  let pages = document.createElement('p');
+  let $element = $("<div>").css({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: "20px",
+  });
 
-  element.style.display = 'flex';
-  element.style.flexDirection = 'column';
-  element.style.alignItems = 'center';
-  element.style.marginTop = '20px';
+  let $title = $("<h4>").text(item.name);
+  let $author = $("<p>").text(`by ${item.authors[0]}`);
+  let $published = $("<p>").text(item.released.substr(0, 4));
+  let $pages = $("<p>").text(`${item.numberOfPages} pages`);
 
-  title.textContent = item.name;
-  author.textContent = `by ${item.authors[0]}`;
-  published.textContent = item.released.substr(0, 4);
-  pages.textContent = `${item.numberOfPages} pages`;
-
-  element.append(title);
-  element.append(author);
-  element.append(published);
-  element.append(pages);
-
-  app.append(element);
+  $element.append($title, $author, $published, $pages);
+  $app.append($element);
 };
 
 const fetchData = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
+  $.getJSON(url)
+    .done((data) => {
       data.forEach((item) => {
         addBookToDOM(item);
       });
     })
-    .catch((error) => {
-      console.log(error);
-      let li = document.createElement('li');
-      li.textContent = `An error occured. Please try again.`;
-      app.append(li);
+    .fail(() => {
+      console.error("An error occurred. Please try again.");
+      let $error = $("<p>").text("An error occurred. Please try again.");
+      $app.append($error);
     })
-    .finally(() => {
-      app.removeChild(loading);
+    .always(() => {
+      $loading.remove();
     });
 };
 
